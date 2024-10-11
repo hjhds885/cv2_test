645fdeebad19a87e46863b00a95fe499a06f6013
hjhds885 <151747511+hjhds885@users.noreply.github.com>
2024-10-11 15:42:44 +0900

    Update st_cv_test.py
    カメラの権限を設定All User

--- st_cv_test.py
+++ st_cv_test.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
 import cv2
+import os
+
+def set_camera_permissions():
+    # カメラデバイスのパスを指定
+    camera_device = '/dev/video0'
+
+    # 666権限を設定
+    command = f'sudo chmod 666 {camera_device}'
+
+    # コマンドを実行
+    os.system(command)
+
+# 関数を呼び出して権限を設定
+set_camera_permissions()
 
 st.title("Streamlit + OpenCV Sample")
 
