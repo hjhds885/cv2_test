79317d67ce57d3f551ee8de1fbc2b0020cc5c76c
hjhds885 <151747511+hjhds885@users.noreply.github.com>
2024-10-11 15:23:11 +0900

    Update st_cv_test.py
    バグ修正

--- st_cv_test.py
+++ st_cv_test.py
@@ -7,13 +7,11 @@ st.title("Streamlit + OpenCV Sample")
 cap = cv2.VideoCapture(0)
 st.header("Image")
 image_placeholder = st.empty()
-if st.button("Start Streaming"):
-    while True:
-        ret, frame = cap.read()
-        if ret:
-            image_placeholder.image(frame, channels="BGR")
-        
-            
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        image_placeholder.image(frame, channels="BGR")
 
 cap.release()
 #cv2.destroyAllWindows()
