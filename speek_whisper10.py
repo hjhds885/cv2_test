693c2c2e0076f2a7483de6373de1b6f515ad9627
hjhds885 <151747511+hjhds885@users.noreply.github.com>
2024-11-12 21:20:52 +0900

    Update speek_whisper10.py
    audio_receiver_size=512,  #1024,

--- speek_whisper10.py
+++ speek_whisper10.py
@@ -274,7 +274,7 @@ def app_sst(
     webrtc_ctx = webrtc_streamer(
         key="speech-to-text",
         mode=WebRtcMode.SENDONLY,
-        audio_receiver_size=1024,
+        audio_receiver_size=512,  #1024,
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
         media_stream_constraints={"video": False, "audio": True},
     )
